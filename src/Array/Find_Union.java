8d243779920355be3805e8110fa831815a5b699d
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-12 08:31:35 +0530

    Examples added

--- src/Array/Find_Union.java
+++ src/Array/Find_Union.java
@@ -1,4 +1,56 @@
 package Array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Find_Union {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 3, 5, 6};
+
+        List<Integer> result = findUnion(arr1, arr2);
+
+        System.out.println("Union of arrays: " + result);
+    }
+
+    public static List<Integer> findUnion(int[] arr1, int[] arr2) {
+        int i = 0, j = 0;
+        List<Integer> union = new ArrayList<>();
+
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                if (union.isEmpty() || union.getLast() != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                if (union.isEmpty() || union.getLast() != arr2[j]) {
+                    union.add(arr2[j]);
+                }
+                j++;
+            } else {
+                if (union.isEmpty() || union.getLast() != arr1[i]) {
+                    union.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+
+        while (i < arr1.length) {
+            if (union.isEmpty() || union.getLast() != arr1[i]) {
+                union.add(arr1[i]);
+            }
+            i++;
+        }
+
+        while (j < arr2.length) {
+            if (union.isEmpty() || union.getLast() != arr2[j]) {
+                union.add(arr2[j]);
+            }
+            j++;
+        }
+
+        return union;
+    }
 }
--- src/Array/Linear_Search.java
+++ src/Array/Linear_Search.java
@@ -1,4 +1,25 @@
 package Array;
 
 public class Linear_Search {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int num = 30;
+
+        int index = findElement(arr, num);
+
+        if (index != -1) {
+            System.out.println("Element " + num + " found at index: " + index);
+        } else {
+            System.out.println("Element " + num + " not found in the array.");
+        }
+    }
+
+    public static int findElement(int[] arr, int num) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == num) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
